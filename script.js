2cdf2efcef52876006e51a4c4ad75db169fcb96c
Léandre Lavenu <leandre.lavenu@outlook.com>
2024-10-16 20:50:04 +0200

    etape 6

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
+let name = prompt (`Prenom :`)
+
 function sayHello(firstname, hour){
 
 
-let messageJour = `Bonjour ${firstname} !`
-let messageNuit = `Bonsoir ${firstname} !`
+    let messageJour = `Bonjour ${firstname} !`
+    let messageNuit = `Bonsoir ${firstname} !`
 
-if (hour >= 18){
-    console.log(messageNuit)
-}
-else {
-    console.log(messageJour)
-}
+    if (hour >= 18){
+        /* console.log(messageNuit) */
+        document.querySelector('h1').innerText = messageNuit;
+    }
+    else {
+        /* console.log(messageJour) */
+        document.querySelector('h1').innerText = messageJour;
+    }
 
 //message = message + firstname = Bonjour ! Beyonce,  Donc pas dingue
 /* message = `Bonjour ${firstname} !`
@@ -17,9 +21,9 @@ console.log(message) */
 
 }
 
-let firstname = 'Beyonce'
-
-
+/* let firstname = 'Beyonce'
 sayHello(firstname, 11)
 sayHello(firstname, 18)
-sayHello(firstname, 17)
+sayHello(firstname, 17) */
+
+sayHello (name, 21)
